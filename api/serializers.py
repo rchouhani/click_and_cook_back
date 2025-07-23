69108ecc7a147117fb0316b0b24b607851154b96
VictorLpr <victorlepron@gmail.com>
2025-07-23 11:57:07 +0200

    add update user

--- api/serializers.py
+++ api/serializers.py
@@ -22,6 +22,18 @@ class CustomUserSerializer(serializers.ModelSerializer):
     
     def create(self, validated_data):
         return CustomUser.objects.create_user(**validated_data)
+    
+    def upadte(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+
+        for attr, value in validated_data.items():
+            setattr(instance,attr, value)
+        
+        if password:
+            instance.set_password(password)
+
+        instance.save()
+        return instance
         
         
 class RecipesSerializer(serializers.ModelSerializer):
