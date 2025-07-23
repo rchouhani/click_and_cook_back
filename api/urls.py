97354d8a588c44296130dda04d6181075d7db8e6
rchouhani <rchouhani@me.com>
2025-07-23 16:34:24 +0200

    ADD : url to login

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RecipesViewSet, CustomUserViewSet
+from .views import RecipesViewSet, CustomUserViewSet, LoginView
 
 router = DefaultRouter()
 
@@ -8,5 +8,6 @@ router.register(r'recipes', RecipesViewSet, basename='recipes')
 router.register(r'user', CustomUserViewSet, basename='user')
 
 urlpatterns = [
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('login/', LoginView.as_view()),
 ]
