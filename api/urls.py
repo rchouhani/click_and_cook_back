b384b9bd54178f7f357f40910e49fa8e0b66e785
rchouhani <rchouhani@me.com>
2025-07-24 15:16:11 +0200

    add : likes and follows urls

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RecipesViewSet, CustomUserViewSet, LoginView
+from .views import RecipesViewSet, CustomUserViewSet, LoginView, LikeViewSet, FollowsViewSet
 
 router = DefaultRouter()
 
 router.register(r'recipes', RecipesViewSet, basename='recipes')
 router.register(r'user', CustomUserViewSet, basename='user')
+router.register(r'likes', LikeViewSet, basename='likes')
+router.register(r'follows', FollowsViewSet, basename='follows')
 
 urlpatterns = [
     path('api/', include(router.urls)),
