aad8a301790758824b0d5e25829e1f19346d0ee4
rchouhani <rchouhani@me.com>
2025-07-24 15:19:05 +0200

    add : function to like once at a time

--- api/views.py
+++ api/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from rest_framework import viewsets, serializers
+from rest_framework import viewsets, serializers, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.contrib.auth import authenticate
 
-from .models import Recipes, CustomUser
-from .serializers import RecipesSerializer, CustomUserSerializer
+from .models import Recipes, CustomUser, Likes, Follows
+from .serializers import RecipesSerializer, CustomUserSerializer, LikesSerializer, FollowsSerializer
 
 
 class CustomUserViewSet(viewsets.ModelViewSet):
@@ -57,4 +57,27 @@ class LoginView(APIView):
             secure=False,
             samesite="Strict"
         )
-        return response     
+        return response
+    
+    
+class LikeViewSet(viewsets.ModelViewSet):
+    queryset = Likes.objects.all()
+    serializer_class = LikesSerializer
+    
+    def create(self, request):
+        user =  request.data.get('user')
+        recipe = request.data.get('recipe')
+        
+        like, created = Likes.objects.get_or_create(user_id=user, recipe_id=recipe)
+        if not created:
+            return Response({'Déjà liké'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            serializer = self.get_serializer(like)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        
+
+
+class FollowsViewSet(viewsets.ModelViewSet):
+    queryset = Follows.objects.all()
+    serializer_class = FollowsSerializer
